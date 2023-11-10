f9bae050ac9f7f9cb52413cc0531aeb8477bd718
admin <admin@admins-MacBook-Pro.local>
2023-11-10 15:43:37 +0600

    HW 2

---
+++ HW2/NextPage.swift
@@ -0,0 +1,15 @@
+//
+//  NextPage.swift
+//  HW2
+//
+//  Created by admin on 10/11/23.
+//
+
+import UIKit
+
+class NextPage : UIViewController {
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
+
---
+++ HW2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by admin on 9/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+            }
+
+    @IBAction func SingBtn(_ sender: Any) {
+    }
+    
+}
+
